dc2e4332ca064a6d1c7c7763045894519847493d
Anjali Mittu <soccerball910@yahoo.com>
2017-01-21 21:35:40 +0000

    Added var to text

--- message.js
+++ message.js
@@ -1,19 +1,42 @@
+function QueryString() {
+  // This function is anonymous, is executed immediately and
+  // the return value is assigned to QueryString!
+  var query_string = {};
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+        // If first entry with this name
+    if (typeof query_string[pair[0]] === "undefined") {
+      query_string[pair[0]] = decodeURIComponent(pair[1]);
+        // If second entry with this name
+    } else if (typeof query_string[pair[0]] === "string") {
+      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+      query_string[pair[0]] = arr;
+        // If third or later entry with this name
+    } else {
+      query_string[pair[0]].push(decodeURIComponent(pair[1]));
+    }
+  }
+  return query_string;
+}
+
 function getNeed() {
-  var qs = new Querystring();
-  return qs.get("need");
+  var qs = QueryString();
+  return qs.need;
 }
 
 function getGoalM() {
-  var qs = new Querystring();
-  return qs.get("goalM");
+  var qs = QueryString();
+  return qs.goalM;
 }
 
 function getGoalD() {
-  var qs = new Querystring();
-  return qs.get("goalD");
+  var qs = QueryString();
+  return qs.goalD;
 }
 
 function getRate() {
-  var qs = new Querystring();
-  return qs.get("tRate");
+  var qs = QueryString();
+  return qs.tRate;
 }
